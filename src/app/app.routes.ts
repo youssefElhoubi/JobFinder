a2afd191687ee7209bd399595ce9a6fced7dd4d2
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-09 14:15:09 +0100

    feat: add HomeLayout component with routing, styles, and tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeLayout } from './layout/home-layout/home-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeLayout,
+        children: [{
+            path: '',
+            loadComponent: () => import('./pages/home/home').then(m => m.Home)
+        }],
+    }
+];
