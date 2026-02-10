51457b47b6ddf58e6cdb71fb415cf55120c7e854
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-10 15:20:35 +0100

    stoped working on the effect untill I made the job service

---
+++ src/app/core/Stores/Jobs/job.effect.ts
@@ -0,0 +1,6 @@
+import { inject } from "@angular/core";
+import { Actions, createEffect } from "@ngrx/effects";
+
+// const JobEffect = createEffect(
+//     (actions$ = inject(Actions), jobservice = inject(Job))    
+// )
