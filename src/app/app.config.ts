df6b9c1ca9e37e137023272bfd948eda25d1f159
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-11 11:10:18 +0100

    added the config of the store to the app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,17 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { JobEffect } from './core/Stores/Jobs/job.effect';
+import { JobFeature } from './core/Stores/Jobs/job.feature';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideStore(),
-    provideEffects(),
+    provideStore({
+      [JobFeature.name]: JobFeature.reducer
+    }),
+    provideEffects({JobEffect}),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
