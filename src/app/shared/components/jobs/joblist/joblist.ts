f7ac31df241e6fa569e4b32de6ea68c153e0ed2e
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-12 09:11:15 +0100

    feat: integrate NgRx store for job list state management

--- src/app/shared/components/jobs/joblist/joblist.ts
+++ src/app/shared/components/jobs/joblist/joblist.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { JobCardes } from '../job-cardes/job-cardes';
 import { PageNav } from "../page-nav/page-nav";
+import { Store } from '@ngrx/store';
+import { selectJobs, selectLoading } from '../../../../core/Stores/Jobs/job.feature';
 
 @Component({
   selector: 'app-joblist',
@@ -9,5 +11,8 @@ import { PageNav } from "../page-nav/page-nav";
   styleUrl: './joblist.css',
 })
 export class Joblist {
-
+  private store :Store = inject(Store);
+  Jobs$ = this.store.select(selectJobs);
+  loading$ =this.store.select(selectLoading);
+  
 }
