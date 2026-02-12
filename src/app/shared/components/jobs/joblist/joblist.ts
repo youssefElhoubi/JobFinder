5e338dae237791ec9ea2afcfdcbcd3c633a127c3
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-12 09:42:57 +0100

    feat: add job input property to job card component and clean up job list template

--- src/app/shared/components/jobs/joblist/joblist.ts
+++ src/app/shared/components/jobs/joblist/joblist.ts
@@ -3,6 +3,7 @@ import { JobCardes } from '../job-cardes/job-cardes';
 import { PageNav } from "../page-nav/page-nav";
 import { Store } from '@ngrx/store';
 import { selectJobs, selectLoading } from '../../../../core/Stores/Jobs/job.feature';
+import { Result } from '../../../../core/Stores/Jobs/jobs.model';
 
 @Component({
   selector: 'app-joblist',
@@ -11,8 +12,11 @@ import { selectJobs, selectLoading } from '../../../../core/Stores/Jobs/job.feat
   styleUrl: './joblist.css',
 })
 export class Joblist {
-  private store :Store = inject(Store);
+  private store: Store = inject(Store);
+  jobs: Result[] | null = null
   Jobs$ = this.store.select(selectJobs);
-  loading$ =this.store.select(selectLoading);
-  
+  loading$ = this.store.select(selectLoading);
+  ngOnInit() {
+    this.Jobs$.subscribe(data => this.jobs = data?.results ?? null)
+  }
 }
