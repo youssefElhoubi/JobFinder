34a78694d575c95a1f8639f4dbaa470c96972f25
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-12 11:37:59 +0100

    feat: add Angular Material theming and integrate Material components in the application

--- src/app/shared/components/home/hero/hero.ts
+++ src/app/shared/components/home/hero/hero.ts
@@ -1,15 +1,17 @@
 import { Component, ElementRef, inject, SimpleChanges, ViewChild } from '@angular/core';
 import { Joblist } from "../../jobs/joblist/joblist";
 import { FormsModule, NgForm } from '@angular/forms';
-import { JobService } from './../../../../core/services/job/jobService';
+import { AsyncPipe } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { selectLoading } from '../../../../core/Stores/Jobs/job.feature';
+import { selectError, selectJobs, selectLoading } from '../../../../core/Stores/Jobs/job.feature';
 import { JobActions } from '../../../../core/Stores/Jobs/jobs.actions';
-import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
 
 @Component({
   selector: 'app-hero',
-  imports: [Joblist, FormsModule],
+  imports: [Joblist, FormsModule, AsyncPipe, MatProgressSpinnerModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
@@ -18,7 +20,10 @@ export class Hero {
   searchInput: string = '';
   country: string = "";
   perPage: number = 1;
-  isloading: boolean = false;
+  Jobs$ = this.store.select(selectJobs);
+  Loading$ = this.store.select(selectLoading);
+  error$ = this.store.select(selectError);
+
   COUNTRIES = [
     { name: 'Austria', code: 'at' },
     { name: 'Australia', code: 'au' },
@@ -40,20 +45,15 @@ export class Hero {
     { name: 'United States', code: 'us' },
     { name: 'South Africa', code: 'za' }
   ]
-  Loading$ = this.store.select(selectLoading);
-  constructor(private JobService: JobService) { }
-
   onSubmit(form: NgForm) {
     if (form.invalid) {
       return
     }
-    this.Loading$.subscribe(data => this.isloading = data)
     this.store.dispatch(
       JobActions.initLoading({
         country: this.country, page: 1, search: this.searchInput, perpage: this.perPage
       })
     )
-    this.Loading$.subscribe(data => this.isloading = data)
-  
+
   }
 }
