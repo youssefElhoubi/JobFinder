2602687db2517dfdf203be27f55f5fa3e097f059
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-13 09:53:51 +0100

    feat: enhance page navigation component with state management and async pipe integration

--- src/app/shared/components/jobs/page-nav/page-nav.ts
+++ src/app/shared/components/jobs/page-nav/page-nav.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectPage, selectPerPage } from '../../../../core/Stores/url/url.feature';
+import { selectJobs } from '../../../../core/Stores/Jobs/job.feature';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-page-nav',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './page-nav.html',
   styleUrl: './page-nav.css',
 })
 export class PageNav {
+  store = inject(Store);
+  page$ = this.store.select(selectPage)
+  perPage$= this.store.select(selectPerPage)
+  jobs$= this.store.select(selectJobs)
+  pagecount=0;
+  ngOnInit(): void {
+  }
 
 }
