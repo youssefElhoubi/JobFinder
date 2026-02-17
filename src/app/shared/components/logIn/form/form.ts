dcdc649eaa8591ff5aafbc1b2b8d48e982b4997c
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-17 17:51:14 +0000

    feat: implement reactive form for login with validation and submission handling

--- src/app/shared/components/logIn/form/form.ts
+++ src/app/shared/components/logIn/form/form.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
-
+  private fb = inject(FormBuilder)
+  loginForm = this.fb.group({
+    email: ["",[Validators.email,Validators.required]],
+    password: ["",[Validators.required,Validators.minLength(8)]]
+  }) 
+  get email(){
+    return this.loginForm.get("email")
+  }
+  get password(){
+    return this.loginForm.get("password")
+  }
+  onSubmite(){
+    if (this.loginForm.valid) {
+      console.log('Form Submitted!', this.loginForm.value);
+    } else {
+      console.log('Form is not valid');
+      this.loginForm.markAllAsTouched(); // Highlights errors if user clicks submit empty
+    }
+  }
 }
