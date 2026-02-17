2574bfc0a5d847579e26d0f458a1692287bfbc97
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-17 20:06:07 +0000

    feat: add userType interface and implement login service with API integration

--- src/app/core/services/job/auth/login.ts
+++ src/app/core/services/job/auth/login.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../../environment/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Login {
-  
+  private http = inject(HttpClient);
+  url = environment.api;
+  login(email:string,password:string):Observable<userType>{
+    return this.http.get<userType>(this.url,{
+      params:{
+        email,
+        password
+      },
+    })
+  }  
 }
