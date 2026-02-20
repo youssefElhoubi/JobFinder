bd67ad880e08833bb05a2ebc9c7b641a9144582e
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-20 18:19:31 +0000

    fix: correct route path for signed layout in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
         }],
     },
     {
-        path: "home",
+        path: "signed",
         component: SignedLoayout,
         children: [{
             path: '',
