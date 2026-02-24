c39130d23e0e16df008a3afb9cfb17fc733c9f58
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-24 03:25:11 +0000

    feat: enhance pagination controls with page navigation functionality and styling

--- src/app/shared/components/jobs/page-nav/page-nav.ts
+++ src/app/shared/components/jobs/page-nav/page-nav.ts
@@ -1,22 +1,40 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { selectPage, selectPerPage } from '../../../../core/Stores/url/url.feature';
-import { selectJobs } from '../../../../core/Stores/Jobs/job.feature';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { selectPage, selectPerPage } from '../../../../core/Stores/url/url.feature';
+// import { selectTotalJobs } from '...'; // <-- WE NEED THIS
 
 @Component({
   selector: 'app-page-nav',
-  // imports: [AsyncPipe],
+  standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './page-nav.html',
   styleUrl: './page-nav.css',
 })
 export class PageNav {
   store = inject(Store);
-  page$ = this.store.select(selectPage)
-  perPage$= this.store.select(selectPerPage)
-  jobs$= this.store.select(selectJobs)
-  pagecount=0;
-  ngOnInit(): void {
+  
+  page$ = this.store.select(selectPage);
+  perPage$ = this.store.select(selectPerPage);
+  
+  // placeholder for what we need to calculate pages
+  // totalJobs$ = this.store.select(selectTotalJobs); 
+
+  // This will eventually hold our array of numbers: [1, 2, 3, 4]
+  pagesArray$!: Observable<number[]>; 
+  totalPages$!: Observable<number>;
+
+  goToPage(pageNumber: number) {
+    // We will dispatch your store action here
+    console.log('Go to page:', pageNumber);
   }
 
+  prevPage() {
+    // Logic to go back 1
+  }
+
+  nextPage() {
+    // Logic to go forward 1
+  }
 }
